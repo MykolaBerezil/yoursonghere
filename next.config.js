0da639433922e1247ea91af67de1cf88cf75bf73
Shankar Acharya <northbynome@gmail.com>
2026-02-20 20:57:26 +0200

    Add redirect: www to non-www (GoDaddy forward removed, single Railway domain)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      // Redirect www to non-www canonical URL
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.yoursonghere.com',
+          },
+        ],
+        destination: 'https://yoursonghere.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
